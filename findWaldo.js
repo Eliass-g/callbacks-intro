5f1fffcc43145d4cd7d41e53e792e224b7805b2e
Eliass-g <eliass.ghauss@gmail.com>
2023-09-21 16:43:10 -0400

    eslint findWaldo.js

--- findWaldo.js
+++ findWaldo.js
@@ -3,11 +3,11 @@ const findWaldo = function(names, found) {
   names.forEach((name) => {
     if (name === "Waldo") found(index);
     index++;
-  })
-}
+  });
+};
 
 const actionWhenFound = function(index) {
   console.log(`Found Waldo at index ${index}!`);
-}
+};
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
