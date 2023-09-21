b3d511a4a4d02e40f3ee0e042eed9002b8afcc66
Eliass-g <eliass.ghauss@gmail.com>
2023-09-21 16:39:38 -0400

    added findWaldo.js

---
+++ findWaldo.js
@@ -0,0 +1,13 @@
+const findWaldo = function(names, found) {
+  let index = 0;
+  names.forEach((name) => {
+    if (name === "Waldo") found(index);
+    index++;
+  })
+}
+
+const actionWhenFound = function(index) {
+  console.log(`Found Waldo at index ${index}!`);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
